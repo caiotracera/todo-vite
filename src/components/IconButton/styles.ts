e21a122225964c4d6a16eef7a5354d6900725033
Caio Tracera <ctrcra@gmail.com>
2023-07-03 20:11:37 -0300

    refactor(icon button): set max-width and remove padding

--- src/components/IconButton/styles.ts
+++ src/components/IconButton/styles.ts
@@ -6,7 +6,10 @@ export const Wrapper = styled(Button)`
   ${({ theme }) => css`
     background-color: transparent;
 
+    width: 100%;
+    max-width: 24px;
     height: 24px;
+    padding: 0;
 
     border-radius: 4px;
 
